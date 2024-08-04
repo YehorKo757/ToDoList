e572c85f8e48c2199ca22f170c6bdf49446e1c9f
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-04 17:38:27 +0100

    Day 3
    Match - case

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-user_prompt = "Enter a todo: "
+user_prompt = "Type add, show or exit: "
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+    user_action = input(user_prompt)
+
+    match user_action.strip():
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for todo in todos:
+                print(todo)
+        case 'exit':
+            print('Bye!')
+            break
+        case _:
+            print("Hey, you've entered a wrong command")

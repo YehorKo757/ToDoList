6d8b0ca13f191756dd701bae55924371e2e47af3
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-05 15:56:30 +0100

    Day 6
    Added feature to ssve todo list in a txt file

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 user_prompt = "Type add, show, edit, complete or exit: "
-todos = []
 
 while True:
     user_action = input(user_prompt)
 
     match user_action.strip():
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             for index, todo in enumerate(todos):
-                print(f"{index+1}. {todo}")
+                print(f"{index+1}. {todo}", end='')
         case 'edit':
             number_to_edit = int(input("What number of todo you want to edit? "))
             todos[number_to_edit - 1] = input(f"Reenter number {number_to_edit} todo: ")

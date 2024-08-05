add17aa372eee6bb394dc445aabd222a17388f5e
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-05 16:22:20 +0100

    Day 7
    fixed complete and edit parts

--- main.py
+++ main.py
@@ -24,11 +24,27 @@ while True:
             for index, todo in enumerate(todos):
                 print(f"{index+1}. {todo}", end='')
         case 'edit':
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             number_to_edit = int(input("What number of todo you want to edit? "))
             todos[number_to_edit - 1] = input(f"Reenter number {number_to_edit} todo: ")
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case 'complete':
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             number_to_complete = int(input("What number of todo you want to complete? "))
             todos.remove(todos[number_to_complete - 1])
+            
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case 'exit':
             print('Bye!')
             break

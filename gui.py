dd3a1071c2cfa6abcfd4938cf970800ca1327ce3
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-08-07 17:55:57 +0100

    Day 17
    Implement Edit button and updating of input box and list field

--- gui.py
+++ gui.py
@@ -5,9 +5,16 @@ label = FreeSimpleGUI.Text("Type in a to-do")
 input_box = FreeSimpleGUI.InputText(tooltip="Enter todo",
                                     key="todo")
 add_button = FreeSimpleGUI.Button("Add")
+list_box = FreeSimpleGUI.Listbox(values=functions.get_todos(),
+                                 key="todos",
+                                 enable_events=True,
+                                 size=[45,10])
+edit_button = FreeSimpleGUI.Button("Edit")
 
 window = FreeSimpleGUI.Window("My To-Do App",
-                              layout=[[label], [input_box, add_button]],
+                              layout=[[label],
+                                      [input_box, add_button],
+                                      [list_box,edit_button]],
                               font=("Helvetica", 16))
 
 while True:
@@ -17,6 +24,19 @@ while True:
             todos = functions.get_todos()
             todos.append(values["todo"] + "\n")
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case FreeSimpleGUI.WIN_CLOSED:
             break
 
